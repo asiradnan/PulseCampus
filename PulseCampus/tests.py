baa6c92ea0455de93aa6b8d1802cbeafb5206d32
asiradnan <asiradnan23@gmail.com>
2025-05-02 17:28:50 +0600

    My first test was not named correctly lol so i fixed and wrote one new now i have two working test cases

--- PulseCampus/tests.py
+++ PulseCampus/tests.py
@@ -2,6 +2,10 @@ from django.test import TestCase
 from django.urls import reverse
 
 class HomepageTest(TestCase):
-    def homepage_status_code(self):
+    def test_homepage_status_code(self):
         response = self.client.get(reverse('homepage'))
         self.assertEqual(response.status_code, 200)
+
+    def test_root_template_used(self):
+        response = self.client.get("/")
+        self.assertTemplateUsed(response, 'homepage.html')  
