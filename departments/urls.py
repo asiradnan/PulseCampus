ef1fd585d9c58f9434ec6d7c9d2c6e750e82f7e6
asiradnan <asiradnan23@gmail.com>
2025-05-02 21:05:46 +0600

    Remove quotes from block names in base.html and added generic list view in deparments app

--- departments/urls.py
+++ departments/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
+from . import views
 
 app_name = "departments"
 
 urlpatterns = [
-   
+   path("", views.DepartmentListView.as_view(),name="department_list")
 ]
--- departments/views.py
+++ departments/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django.views.generic.list import ListView
+from .models import Department
 
-# Create your views here.
+class DepartmentListView(ListView):
+    model = Department
