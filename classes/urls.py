94a90a6004fba88a71742549a835c8ed60fe4e87
asiradnan <asiradnan23@gmail.com>
2025-05-04 13:38:19 +0600

    Class update

--- classes/urls.py
+++ classes/urls.py
@@ -4,5 +4,9 @@ from . import views
 app_name = 'classes'    
 
 urlpatterns = [
-
+    path('create/',views.CreateClassView.as_view(),name='class_create'),
+    path('class_list/',views.ClassListView.as_view(),name='class_list'),
+    path('<str:pk>/',views.ClassDetailView.as_view(),name='class_detail'),
+    path('<str:pk>/update/',views.ClassUpdateView.as_view(),name='class_update'),
+    path('<str:pk>/delete/',views.ClassDeleteView.as_view(),name='class_delete'),
 ]
--- classes/views.py
+++ classes/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from django.views.generic.edit import CreateView, UpdateView
+from django.views.generic import ListView, DetailView, DeleteView
+from . import models
 
-# Create your views here.
+class CreateClassView(CreateView):
+    model = models.Class
+    fields = "__all__"
+
+class ClassListView(ListView):
+    model = models.Class
+
+class ClassDetailView(DetailView):
+    model = models.Class    
+
+class ClassUpdateView(UpdateView):
+    model = models.Class
+    fields = "__all__"
+
+class ClassDeleteView(DeleteView):
+    model = models.Class
