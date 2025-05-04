c038da8fffd01838f67293750411553225918976
asiradnan <asiradnan23@gmail.com>
2025-05-04 16:56:08 +0600

    allowed host in django for ec2

--- PulseCampus/settings.py
+++ PulseCampus/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-1l5!%r1&4!#nkr&e&j7+tat6al!huz(&*_5_&$sf1(*0_0_02_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
