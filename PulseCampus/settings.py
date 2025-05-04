072e907b7ae348cc4954bad2b29fd66e8a57c2d8
asiradnan <asiradnan23@gmail.com>
2025-05-04 18:38:44 +0600

    Changed static root

--- PulseCampus/settings.py
+++ PulseCampus/settings.py
@@ -124,7 +124,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
-STATIC_ROOT = "var/www/pulsecampus_staticfiles" #For production 
+STATIC_ROOT = "staticfiles" #For production 
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
