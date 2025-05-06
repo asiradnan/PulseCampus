af69c156b67b90cf7b9a72becab81f4305f3421c
asiradnan <asiradnan23@gmail.com>
2025-05-06 19:42:55 +0600

    Created post and comment models

--- community/models.py
+++ community/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Post(models.Model):
+    posted_by=models.ForeignKey(User,on_delete=models.CASCADE)
+    content=models.TextField(max_length=10000)
+    image = models.ImageField(upload_to='images/',null=True,blank=True)
+    time = models.DateTimeField(auto_now_add=True, editable=False)
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    commented_by = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.CharField(max_length=1000)
+    time = models.DateTimeField(auto_now_add=True, editable=False)
