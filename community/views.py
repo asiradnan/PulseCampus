f61018dc30c80e65ba958cb40f24bcf402573e94
asiradnan <asiradnan23@gmail.com>
2025-05-06 19:47:17 +0600

    Added crud CBV for community

--- community/views.py
+++ community/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
 from django.views.generic.edit import CreateView, UpdateView
-from django.views.generic import DeleteView, DetailView
+from django.views.generic import DeleteView, DetailView, ListView
+from .models import Post, Comment
+
+class PostCreateView(CreateView):
+    model = Post
+    fields = '__all__'
+
+class PostUpdateView(UpdateView):
+    model = Post
+    fields = '__all__'
+
+class PostDetailView(DetailView):
+    model = Post
+
+class PostListView(ListView):
+    model = Post
+
+class PostDeleteView(DeleteView):
+    model = Post
