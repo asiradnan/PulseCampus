6f23a5144354503c6089bd5c0647029f0f4d9265
asiradnan <asiradnan23@gmail.com>
2025-05-14 18:24:34 +0600

    added file extension validator:

--- notice/models.py
+++ notice/models.py
@@ -1,12 +1,13 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.urls import reverse
+from django.core.validators import FileExtensionValidator
 
 class Notice(models.Model):
     posted_by=models.ForeignKey(User,on_delete=models.CASCADE)
     title=models.CharField(max_length=200)
     content=models.TextField(max_length=10000)
-    pdf_file = models.FileField(upload_to='notices/', null=True, blank=True)
+    pdf_file = models.FileField(upload_to='notices/', null=True, blank=True, validators=[FileExtensionValidator(['pdf'])])
     date=models.DateField(auto_now_add=True, editable=False)
 
     def __str__(self):
