b3998eb31f5aa7dde0a1c13f6b4008d7328b1d45
asiradnan <asiradnan23@gmail.com>
2025-05-16 02:01:10 +0600

    updated prod url in email

--- users/tasks.py
+++ users/tasks.py
@@ -5,7 +5,7 @@ from django.core.mail import send_mail
 def send_confirmation_email(email_address, token):
     email_sent = send_mail(
             'Welcome to PulseCampus',
-            'Thank you for registering on PulseCampus! Please verify your email address by clicking the link below: http://127.0.0.1:8000/users/verify_email/{} Note that the link is valid for 24 hours. '.format(token),
+            'Thank you for registering on PulseCampus! Please verify your email address by clicking the link below: https://pulsecampus.asiradnan.com/users/verify_email/{} Note that the link is valid for 24 hours. '.format(token),
             'verify-pulsecampus@asiradnan.com',
             [email_address],
             fail_silently=False,
@@ -15,7 +15,7 @@ def send_confirmation_email(email_address, token):
 def send_reset_email(email_address, token):
     email_sent = send_mail(
             'Password Reset',
-            'Click the link below to reset your password: http://127.0.0.1:8000/users/reset_password/{}'.format(token),
+            'Click the link below to reset your password: https://pulsecampus.asiradnan.com/users/reset_password/{}'.format(token),
             'verify-pulsecampus@asiradnan.com',
             [email_address],
             fail_silently=False
