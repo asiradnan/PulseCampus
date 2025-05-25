e2f54ec4fb8bf9f8c7c36077d0d2c752b5b01df8
asiradnan <asiradnan23@gmail.com>
2025-05-25 11:53:20 +0600

    updated to get access to s3

--- PulseCampus/settings_test.py
+++ PulseCampus/settings_test.py
@@ -1,7 +1,5 @@
 from .settings import *
 
-DEFAULT_FILE_STORAGE = 'django.core.files.storage.FileSystemStorage'
-MEDIA_ROOT = BASE_DIR / 'test_media'
 # Optional: Speed up tests by using faster password hashing
 PASSWORD_HASHERS = [
     'django.contrib.auth.hashers.MD5PasswordHasher',
