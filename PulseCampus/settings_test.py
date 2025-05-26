184ec7522553af850787b1e3d330a1974ea9dec0
asiradnan <asiradnan23@gmail.com>
2025-05-26 14:40:24 +0600

    db in settings_test

--- PulseCampus/settings_test.py
+++ PulseCampus/settings_test.py
@@ -7,3 +7,14 @@ PASSWORD_HASHERS = [
 
 # Optional: Disable email backend or use console backend
 EMAIL_BACKEND = 'django.core.mail.backends.locmem.EmailBackend'
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'test_db',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'localhost',
+        'PORT': '5432'
+    }
+}
